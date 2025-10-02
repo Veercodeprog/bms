dce913ab48e9a102cd47e874285376ddaf7b6d13
Veer Pratap Singh <veerpratapsingh@Veers-MacBook-Air.local>
2025-10-02 21:33:30 +0530

    test deployment

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ import { client } from "@repo/db/client";
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hi There");
+  res.send("Hiii There");
 });
 
 app.post("/signup", async (req, res) => {
@@ -27,6 +27,6 @@ app.get("/", (req, res) => {
   res.send("Hi There");
 });
 
-app.listen(3002, '0.0.0.0', () => {
-  console.log('Web app running on port 3002');
+app.listen(3002, "0.0.0.0", () => {
+  console.log("Web app running on port 3002");
 });
